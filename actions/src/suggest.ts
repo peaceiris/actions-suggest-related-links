e6d9a8498a795e4e796a31eef02af041d6d0a435
peaceiris <30958501+peaceiris@users.noreply.github.com>
2020-09-17 22:23:21 +0900

    feat: get body of new issue

--- actions/src/suggest.ts
+++ actions/src/suggest.ts
@@ -1,8 +1,11 @@
 import {GitHubAPI} from './github-api';
 import {Inputs} from './interfaces';
 import {context} from '@actions/github/lib/utils';
+import {md2text, removeSymbols} from './preprocess';
 
-export async function suggest(inps: Inputs, body: string): Promise<void> {
+export async function suggest(inps: Inputs): Promise<void> {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const body: string = (context.issue as any).body;
   const githubAPI = new GitHubAPI(inps.GithubToken, context.repo.owner, context.repo.repo);
-  githubAPI.createComment(body);
+  githubAPI.createComment(removeSymbols(md2text(body)));
 }
