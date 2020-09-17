4d1b383411e399bcc83ae9a7658636789ddf4378
peaceiris <30958501+peaceiris@users.noreply.github.com>
2020-09-18 00:55:58 +0900

    fix: Body cannot be blank

--- actions/src/suggest.ts
+++ actions/src/suggest.ts
@@ -7,5 +7,12 @@ export async function suggest(inps: Inputs): Promise<void> {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const body = `${(context.payload.issue as any).body}`;
   const githubAPI = new GitHubAPI(inps.GithubToken, context.repo.owner, context.repo.repo);
-  await githubAPI.createComment(removeSymbols(md2text(body)));
+  const suggestBody = (() => {
+    if (body === '') {
+      return 'context body is empty';
+    } else {
+      return removeSymbols(md2text(body));
+    }
+  })();
+  await githubAPI.createComment(suggestBody);
 }
