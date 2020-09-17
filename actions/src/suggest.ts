6a15b4bff77859a704a91aad9f71376a4d2bfaf3
peaceiris <30958501+peaceiris@users.noreply.github.com>
2020-09-17 23:20:30 +0900

    fix: context.payload.issue

--- actions/src/suggest.ts
+++ actions/src/suggest.ts
@@ -5,7 +5,7 @@ import {md2text, removeSymbols} from './preprocess';
 
 export async function suggest(inps: Inputs): Promise<void> {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const body = `${(context.issue as any).body}`;
+  const body = `${(context.payload.issue as any).body}`;
   const githubAPI = new GitHubAPI(inps.GithubToken, context.repo.owner, context.repo.repo);
   await githubAPI.createComment(removeSymbols(md2text(body)));
 }
