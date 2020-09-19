bbb9f14abddea5b8dee5c00320a1c40dc606a46a
Shohei Ueda <30958501+peaceiris@users.noreply.github.com>
2020-09-20 06:34:04 +0900

    refactor: enhance suggestion format (#12)

--- actions/src/suggest.ts
+++ actions/src/suggest.ts
@@ -14,16 +14,19 @@ export async function suggest(inps: Inputs, tmpDir: string): Promise<void> {
   }
   for (let i = 0; i < topNcount; i++) {
     commentBody +=
-      `- (${results[i].probability.toFixed(3)}) ` +
+      `- \`${results[i].probability.toFixed(3)}\` ` +
       `[${results[i].title}](${results[i].html_url})\n`;
   }
   if (inps.Unclickable) {
     commentBody += '```\n';
   }
+  const logURL = `${process.env['GITHUB_SERVER_URL']}/${process.env['GITHUB_REPOSITORY']}/actions/runs/${process.env['GITHUB_RUN_ID']}`;
   commentBody +=
-    `[Log](${process.env['GITHUB_SERVER_URL']}/` +
-    `${process.env['GITHUB_REPOSITORY']}/` +
-    `actions/runs/${process.env['GITHUB_RUN_ID']})\n`;
+    `\n<div align="right">` +
+    `<a href="${logURL}">Log</a>` +
+    ` | ` +
+    `<a href="https://github.com/peaceiris/actions-suggest-related-links">Bot Usage</a>` +
+    `</div>\n`;
 
   const githubAPI = new GitHubAPI(inps.GithubToken, context.repo.owner, context.repo.repo);
   await githubAPI.createComment(commentBody);
